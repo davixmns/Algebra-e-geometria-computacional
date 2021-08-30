dfc87d5114551048a9e663150ba2de12c85f2194
Tarcizo Neto <tarcizon12@gmail.com>
2021-08-30 17:13:44 -0300

    Ajuste de arquivos

--- Cenario 1/code.js
+++ Cenario 1/code.js
@@ -22,6 +22,25 @@ class Matrix {
 
 
 }
+class Vector {
+    //dim = dimensão do Vetor
+    //elements = elementos do veto
+    constructor(dim, elements) {
+        var v = []
+        for (i = 0; i < dim; i++) {
+            v[i] = elements
+        }
+        console.log(v)
+    }
+    get(i) {
+        console.log(v[i])
+    }
+    set(i, value) {
+        v[i] = value
+    }
+}
+
+
 var array = [
     [2, 2, 3],
     [1, 2, 3]
