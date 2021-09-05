cc6ac385f2aa9300e5c48ad0115accc39886cfba
Davi Ximenes <dropperdavi@edu.unifor.br>
2021-09-05 20:50:40 -0300

    iniciando testes da vector

--- Cenario 1/code.js
+++ Cenario 1/code.js
@@ -4,69 +4,54 @@ class Matrix {
     //rows = linhas, coluns = colunas, elements= elementos
 
     constructor(rows, cols, elements) {
-        this.rows = rows;
-        this.cols = cols
-        this.elements = elements
-        this.matriz = 0;
-    }
-
-    criador() {
         var m = [];
-        for (var i = 0; i < this.rows; i++) {
+        for (var i = 0; i < rows; i++) {
             m[i] = [];
 
-            for (var j = 0; j < this.cols; j++) {
-                m[i][j] = this.elements[i][j];
+            for (var j = 0; j < cols; j++) {
+                m[i][j] = elements[i][j];
             }
         }
-        return this.m = m;
+        this.m = m;
     }
-    get(i, j) {
-        if (i > this.rows || j > this.cols) {
-            return console.log("Parametro invalido")
-        } else {
-            return this.m[i][j];
-        }
 
+    get(i, j) {
+        return this.m[i][j];
     }
 
     set(i, j, value) {
-        if (i > this.rows || j > this.cols) {
-            return console.log("Parametro invalido")
-        } else {
-            return this.m[i][j] = value;
-        }
+        this.m[i][j] = value;
     }
 
 }
+
 class Vector {
     //dim = dimensão do Vetor
     //elements = elementos do Vetor
     constructor(dim, elements) {
         var v = []
-        elements = parseInt(lines.shift());
         for (i = 0; i < dim; i++) {
             v[i] = elements
         }
     }
-    get(i){
+    get(i) {
         this.v[i]
     }
-    set(i, value){
+    set(i, value) {
         this.v[i] = value
     }
 }
 
 class LinearAlgebra {
-    constructor() {}
+    constructor() { }
 
-    sum(a, b) {}
+    sum(a, b) { }
 
-    times(a, b) {}
+    times(a, b) { }
 
-    dot(a, b) {}
+    dot(a, b) { }
 
-    gauss(a) {}
+    gauss(a) { }
 
-    solve(a) {}
+    solve(a) { }
 }
--- Cenario 1/davitestes.js
+++ Cenario 1/davitestes.js
@@ -2,16 +2,23 @@ class Vector {
     //dim = dimensão do Vetor
     //elements = elementos do veto
     constructor(dim, elements) {
+        this.dim = dim
+        this. elements = elements
+    }
+    criador() {
         var v = []
-        elements = parseInt(lines.shift());
-        for (i = 0; i < dim; i++) {
+        for (var i = 0; i < dim; i++) {
             v[i] = elements
         }
     }
     get(i){
-        this.v[i]
+        return this.v[i]
     }
     set(i, value){
         this.v[i] = value
     }
 }
+var vetor = [1,2,3];
+var objeto = new Vector(3, vetor);
+console.log(Vector.get(2))
+console.log(Vector)
