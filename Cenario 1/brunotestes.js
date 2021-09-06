95d99391732ef36ff22e6eaf78605c764650ef2d
BrunoppNeves <82058653+BrunoppNeves@users.noreply.github.com>
2021-09-06 13:14:23 -0300

    Update brunotestes.js

---
+++ Cenario 1/brunotestes.js
@@ -0,0 +1,26 @@
+class LinearAlgebra {
+
+    transpose(criador) {
+        let transposta = [
+            [criador[i].length][criador[j].length]
+        ];
+        for (let i = 0; i < transposta[i].length; i++) {
+            for (let j = 0; j < transposta[j].length; j++) {
+                transposta[i][j] = criador[j][i];
+            }
+        }
+        return transposta;
+    }
+
+    sum(a, b) {}
+
+    times(a, b) {}
+
+    dot(a, b) {}
+
+    gauss(a) {}
+
+    solve(a) {}
+
+}
+console.log("teste");
