5a27a657b72d7617f16f5521a6b1e653f1ae7773
Davi Ximenes <dropperdavi@edu.unifor.br>
2021-09-07 23:12:26 -0300

    comentários na vector 3

--- Cenario 1/code.js
+++ Cenario 1/code.js
@@ -37,16 +37,13 @@ class Matrix {
 }
 
 class Vector {
-    //dim = dimensão do Vetor
-    //elements = elementos do veto
-    //objeto da classe = vetor
-    constructor(dim, elements) {
+
+    constructor(dim, elements) { //dim = dimensão do array | elements = elementos
         this.dim = dim
         this.elements = elements
-        this.vetor = 0
     }
 
-    criador() {
+    criador() { //método que cria o vetor
         var v = []
         for (var i = 0; i < this.dim; i++) {
             v[i] = this.elements
@@ -54,15 +51,15 @@ class Vector {
         return this.v = v;
     }
 
-    get(i) {
-        if(i > this.dim){
+    get(i) { //método que pega o valor guardado no índice digitado
+        if(i > this.dim){ //se indice digitado for maior que a dimensão do array
             return console.log("Parâmetro inválido")
         }else{
             return this.v[i]
         }
     }
-    set(i, value) {
-        if(i > this.v.length){
+    set(i, value) { //método que substitui no índice i o valor digitado
+        if(i > this.dim){ //se o indice digitado for maior que a dimensão do array
             return console.log("Parâmetro inválido")
         } else {
             this.v[i] = value
