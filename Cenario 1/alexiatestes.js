e3a7efa6f240c08543d9795fcb3a666b22959e98
alexiagt <82297754+alexiagt@users.noreply.github.com>
2021-09-08 16:20:42 -0300

    Update alexiatestes.js
    testes

--- Cenario 1/alexiatestes.js
+++ Cenario 1/alexiatestes.js
@@ -1,1 +1,102 @@
-//teste
+class Matrix {
+    //rows = linhas, coluns = colunas, elements= elementos
+
+    constructor(rows, cols, elements) {
+        this.rows = rows;
+        this.cols = cols;
+        this.elements = elements;
+        this.matriz = 0;
+    }
+
+    criador() {
+        var m = [];
+        for (var i = 0; i < this.rows; i++) {
+            m[i] = [];
+
+            for (var j = 0; j < this.cols; j++) {
+                m[i][j] = this.elements[i][j];
+            }
+        }
+        return this.m = m;
+    }
+    get(i, j) {
+        if (i > this.rows || j > this.cols) {
+            return console.log("Parametro invalido")
+        } else {
+            return this.m[i][j];
+        }
+
+    }
+    set(i, j, value) {
+        if (i > this.rows || j > this.cols) {
+            return console.log("Parametro invalido")
+        } else {
+            return this.m[i][j] = value;
+        }
+    }
+}
+
+class Vector {
+
+    constructor(dim, elements) { //dim = dimensão do array | elements = elementos
+        this.dim = dim
+        this.elements = elements
+    }
+
+    criador() { //método que cria o vetor
+        var v = []
+        for (var i = 0; i < this.dim; i++) {
+            v[i] = this.elements
+        }
+        return this.v = v;
+    }
+
+    get(i) { //método que pega o valor guardado no índice digitado
+        if(i > this.dim){ //se indice digitado for maior que a dimensão do array
+            return console.log("Parâmetro inválido")
+        }else{
+            return this.v[i]
+        }
+    }
+    set(i, value) { //método que substitui no índice i o valor digitado
+        if(i > this.dim){ //se o indice digitado for maior que a dimensão do array
+            return console.log("Parâmetro inválido")
+        } else {
+            this.v[i] = value
+        }
+    }
+}
+
+var vetor = new Vector(3, 0); //instanciação do objeto com parâmetros do método construtor
+vetor.criador() //Criação do array
+
+
+class LinearAlgebra {
+    constructor(objeto1, objeto2) {
+      this.objeto1 = objeto1;
+      this.objeto2 = objeto2;
+    }
+    // objeto1: objeto de Matrix ou Vector
+    // objeto2: objeto de Matrix ou Vector
+
+    transpose(objeto1) {
+      var o = this.objeto1;
+      var t = [];
+      t = reverse(o);
+      return this.t;
+    }
+
+    sum(a, b) {}
+
+    times(a, b) {}
+
+    dot(a, b) {}
+
+    gauss(a) {}
+
+    solve(a) {}
+}
+
+var transposta = new LinearAlgebra(vetor.v, 0);
+transposta.transpose();
+console.log(transposta.t);
