63ae03ebfbbd3436e2079276823f818eeac75d4c
Davi Ximenes <dropperdavi@edu.unifor.br>
2021-09-25 20:35:04 -0300

    conserto da Vector

--- Cenario 1/davitestes.js
+++ Cenario 1/davitestes.js
@@ -1,48 +1,3 @@
-class Matrix {
-    //rows = linhas, coluns = colunas, elements= elementos
-
-    constructor(rows, cols, elements) {
-        this.rows = rows;
-        this.cols = cols
-        this.elements = elements
-        this.matriz = 0;
-    }
-
-
-
-    criador() {
-        if (this.elements.length == this.rows && this.elements[0].length == this.cols) {
-            var m = [];
-            for (var i = 0; i < this.rows; i++) {
-                m[i] = [];
-
-                for (var j = 0; j < this.cols; j++) {
-                    m[i][j] = this.elements[i][j];
-                }
-            }
-            return this.m = m;
-        } else console.log('Array nao suportado')
-
-    }
-    get(i, j) {
-        if (i > this.rows || j > this.cols) {
-            return console.log("Parametro invalido")
-        } else {
-            return this.m[i][j];
-        }
-
-    }
-
-    set(i, j, value) {
-        if (i > this.rows || j > this.cols) {
-            return console.log("Parametro invalido")
-        } else {
-            return this.m[i][j] = value;
-        }
-    }
-
-}
-
 class Vector {
 
     constructor(dim, elements) { //dim = dimensão do array | elements = elementos
@@ -51,12 +6,19 @@ class Vector {
     }
 
     criador() { //método que cria o vetor
-        var v = []
-        for (var i = 0; i < this.dim; i++) {
-            v[i] = this.elents[j]
-            j++
+        if (this.elements.length == this.dim) {
+            var v = []
+            var j = 0;
+            for (var i = 0; i < this.dim; i++) {
+                v[i] = this.elements[j]
+                j++
+            }
+            return this.v = v
         }
-        return this.v = v;
+        else{
+            console.log("Parâmetro inválido");
+        }
+
     }
 
     get(i) { //método que pega o valor guardado no índice digitado
@@ -74,99 +36,7 @@ class Vector {
         }
     }
 }
-
-class LinearAlgebra {
-    constructor(objeto1, objeto2) {
-        this.objeto1 = objeto1;
-        this.objeto2 = objeto2;
-    }
-    // objeto1: objeto de Matrix ou Vector
-    // objeto2: objeto de Matrix ou Vector
-
-    transpose(objeto1) {
-        var o = this.objeto1;
-        var t = [];
-        t = reverse(o);
-        return this.t;
-    }
-
-    sum(a, b) {
-        if (Array.isArray(a) == true && Array.isArray(b) == true) {
-
-            var linhas = a.length;
-            var colunas = a[0].length;
-            var linahsb = b.length;
-            var colunasb = b[0].length;
-
-            if (linhas == linahsb && colunas == colunasb) {
-                var aux = [];
-                if (colunas > 0) {
-
-                    for (var i = 0; i < linhas; i++) {
-                        aux[i] = [];
-                        for (var x = 0; x < colunas; x++) {
-                            aux[i][x] = a[i][x] + b[i][x]
-                        }
-                    }
-
-                    return aux;
-                } else if (colunas == undefined) {
-                    for (var y = 0; y < linhas; y++) {
-                        aux[y] = a[y] + b[y]
-                    }
-
-                    return aux;
-                }
-            } else console.log("Operação impossível")
-        } else console.log("Parametro invalido")
-    }
-
-    times(a, b) {}
-
-    dot(a, b) {
-
-
-        if (Array.isArray(a) == true && Array.isArray(b) == true && a[0].length != undefined && b[0].length != undefined) {
-            var linhas = a.length,
-                colunas = a[0].length,
-                linahsb = b.length,
-                colunasb = b[0].length;
-            if (colunas == linahsb) {
-                var m = [];
-                for (var x = 0; x < linhas; x++) {
-                    m[x] = [];
-                    for (var y = 0; y < colunasb; y++) {
-                        m[x][y] = 0;
-                    }
-                }
-                for (var i = 0; i < linhas; i++) {
-                    for (var j = 0; j < colunasb; j++) {
-                        for (var k = 0; k < colunas; k++) {
-                            m[i][j] = m[i][j] + a[i][k] * b[k][j];
-                        }
-                    }
-                }
-
-                return m;
-            } else {
-                return " Operação impossivel "
-            }
-
-        } else {
-            return "Aceito apenas matrizes"
-        }
-
-    }
-
-    gauss(a) {}
-
-    solve(a) {}
-}
-var elemVector = 1
-var objVector = new Vector(3, elem)
-var objMatrix = new Matrix()
-function criarVetor(){
-    objVector.criador()
-    return console.log(objVector.v)
-}
-criarVetor()
+var elements = [1, 2, 3]
+var obj = new Vector(3, elements)
+obj.criador()
+console.log(obj.v)
