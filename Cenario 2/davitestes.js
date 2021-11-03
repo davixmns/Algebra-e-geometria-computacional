bd4b34c08f6d5e7fd114e24321a3d621822164ed
Davi Ximenes <davixmnsl@gmail.com>
2021-11-03 18:30:05 -0300

    incio reflection 2dx

--- Cenario 2/davitestes.js
+++ Cenario 2/davitestes.js
@@ -5,7 +5,7 @@ class Vector {
         this.elements = elements
     }
 
-    criador() {
+    criador() { //método que cria o vetor
         if (this.elements.length == this.dim) {
             var v = []
             var j = 0;
@@ -20,22 +20,218 @@ class Vector {
 
     }
 
-    get(i) { 
-        if (i > this.dim) {
+    get(i) { //método que pega o valor guardado no índice digitado
+        if (i > this.dim) { //se indice digitado for maior que a dimensão do array
             return console.log("Parâmetro inválido")
         } else {
             return this.v[i]
         }
     }
-    set(i, value) { 
-        if (i > this.dim) { 
+    set(i, value) { //método que substitui no índice i o valor digitado
+        if (i > this.dim) { //se o indice digitado for maior que a dimensão do array
             return console.log("Parâmetro inválido")
         } else {
             this.v[i] = value
         }
     }
 }
+class LinearAlgebra {
+    constructor(objeto1, objeto2) {
+        this.objeto1 = objeto1;
+        this.objeto2 = objeto2;
+    }
+    // objeto1: objeto de Matrix ou Vector
+    // objeto2: objeto de Matrix ou Vector
 
-class Reflection {
+    transpose(a) {
 
+        var linhas = a.length;
+        var colunas = a[0].length;
+
+        if (Array.isArray(a) == true && colunas != undefined) {
+            var newArray = [];
+            for (var i = 0; i < a.length; i++) {
+                newArray.push([]);
+            };
+
+            for (var i = 0; i < a.length; i++) {
+                for (var j = 0; j < a.length; j++) {
+                    newArray[j].push(a[i][j]);
+                };
+            };
+            return newArray;
+        } else if (Array.isArray(a) == true && colunas == undefined) {
+            var resul = [];
+            resul = a.reverse();
+            return resul;
+        } else console.log("operação impossivel")
+    }
+
+    sum(a, b) {
+        if (Array.isArray(a) == true && Array.isArray(b) == true) {
+
+            var linhas = a.length;
+            var colunas = a[0].length;
+            var linahsb = b.length;
+            var colunasb = b[0].length;
+
+            if (linhas == linahsb && colunas == colunasb) {
+                var aux = [];
+                if (colunas > 0) {
+
+                    for (var i = 0; i < linhas; i++) {
+                        aux[i] = [];
+                        for (var x = 0; x < colunas; x++) {
+                            aux[i][x] = a[i][x] + b[i][x]
+                        }
+                    }
+
+                    return aux;
+                } else if (colunas == undefined) {
+                    for (var y = 0; y < linhas; y++) {
+                        aux[y] = a[y] + b[y]
+                    }
+
+                    return aux;
+                }
+            } else console.log("Operação impossível")
+        } else console.log("Parametro invalido")
+    }
+
+    times(a, b) {
+        var timesM = [];
+
+        if (Array.isArray(a) == false) {
+            for (var i = 0; i < b.length; i++) {
+                timesM[i] = [];
+                for (var j = 0; j < b[0].length; j++) {
+                    timesM[i][j] = b[i][j] * a;
+                }
+            }
+            return timesM;
+        } else if (a[0].length == undefined) {
+            for (var y = 0; y < a.length; y++) {
+                timesM[y] = a[y] * b[y]
+            }
+
+            return timesM;
+        } else {
+
+            for (var i = 0; i < b.length; i++) {
+                timesM[i] = [];
+                for (var j = 0; j < b[0].length; j++) {
+                    timesM[i][j] = b[i][j] * a[i][j];
+                }
+            }
+            return timesM;
+        }
+
+    }
+
+    dot(a, b) {
+
+
+        if (Array.isArray(a) == true && Array.isArray(b) == true && a[0].length != undefined && b[0].length != undefined) {
+            var linhas = a.length,
+                colunas = a[0].length,
+                linahsb = b.length,
+                colunasb = b[0].length;
+            if (colunas == linahsb) {
+                var m = [];
+                for (var x = 0; x < linhas; x++) {
+                    m[x] = [];
+                    for (var y = 0; y < colunasb; y++) {
+                        m[x][y] = 0;
+                    }
+                }
+                for (var i = 0; i < linhas; i++) {
+                    for (var j = 0; j < colunasb; j++) {
+                        for (var k = 0; k < colunas; k++) {
+                            m[i][j] = m[i][j] + a[i][k] * b[k][j];
+                        }
+                    }
+                }
+
+                return m;
+            } else {
+                return " Operação impossivel "
+            }
+
+        } else {
+            return "Aceito apenas matrizes"
+        }
+
+    }
+
+    gauss(a) {
+        var aux;
+        var i, j, k, c;
+        var gaussM = a.elements;
+
+        for (i = 0; i < a.rows - 1; i++) {
+            for (j = i + 1; j < a.rows; j++) {
+                if (gaussM[i][i] == 0) {
+                    for (k = i; k < a.cols; k++) {
+                        aux = gaussM[i][k];
+                        gaussM[i][k] = gaussM[i + 1][k];
+                        gaussM[i + 1][k] = aux;
+                    }
+                }
+
+                if (gaussM[j][i] == 0) {
+                    k = gaussM[j][i] / gaussM[i][i];
+                    for (c = i; c < a.cols; c++) {
+                        gaussM[j][c] = gaussM[j][c] - k * gaussM[i][c];
+                    }
+                }
+            }
+        }
+        return gaussM;
+    }
+
+    solve(a) {
+        var res = [];
+        var i, j, k, c;
+
+        var cal = new LinearAlgebra()
+        var soveMatriz = cal.gauss(a)
+
+        for (i = a.rows - 1; i > 0; i--) {
+            for (j = i - 1; j >= 0; j--) {
+                k = soveMatriz[j][i] / soveMatriz[i][i];
+                for (c = i; c < a.cols; c++) {
+                    soveMatriz[j][c] = soveMatriz[j][c] - k * soveMatriz[i][c];
+                }
+            }
+        }
+
+        for (i = 0; i < a.rows; i++) {
+            soveMatriz[i][a.cols - 1] = soveMatriz[i][a.cols - 1] / soveMatriz[i][i];
+            soveMatriz[i][i] = 1;
+
+            res[i] = soveMatriz[i][a.cols - 1]
+        }
+        return res;
+    }
 }
+class Reflection {
+    
+    reflection2DX(vector){
+        var elements = [
+            [-1, 0],
+            [0, 1]
+        ]
+        var objLA = new LinearAlgebra()
+        return objLA.times(elements, vector)
+    }
+
+    reflection2DY(){
+
+    }
+}
+var vetor = [
+    [-10],
+    [89]
+]
+var objR = new Reflection()
+console.log(objR.reflection2DX(vetor))
